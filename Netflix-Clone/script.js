d6784d6a389afb318d3278e9da3818527c0abd5c
vamsi jagatha <vamsijagatha31@gmail.com>
2024-06-12 16:51:20 +0530

    Netflix-Clone

---
+++ Netflix-Clone/script.js
@@ -0,0 +1,35 @@
+let userEmail = document.getElementById("userEmail");
+let accordionNone = document.getElementById("accordionNone");
+let plusSign = document.getElementById("plusSign");
+let listContainer = document.getElementById("listContainer");
+
+
+const accordionButtons = document.querySelectorAll(".accordion-button");
+
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+  
+    const accordionContent = this.parentElement.nextElementSibling;
+    const isCurrentlyVisible =
+      !accordionContent.classList.contains("accordion-none");
+
+    const allAccordionBlocks = document.querySelectorAll(".accordion-block");
+    allAccordionBlocks.forEach((block) => {
+      block.classList.add("accordion-none");
+    });
+
+    const allPlusSigns = document.querySelectorAll(".plus-sign");
+    allPlusSigns.forEach((sign) => {
+      sign.classList.remove("plus-rotate");
+    });
+
+    if (!isCurrentlyVisible) {
+      accordionContent.classList.toggle("accordion-none");
+
+      const plusSign = this.querySelector(".plus-sign");
+      if (plusSign) {
+        plusSign.classList.toggle("plus-rotate");
+      }
+    }
+  });
+});
